8cfefff9a8570fb06d7d0b95503bdc1013073757
andrei.diaconescu <andrei.diaconescu@anais.digital>
2024-04-12 14:52:56 +0300

    work on Area prefs choosing by user, and got stuck with state refresh issue

--- src/components/ChoosePrefsWithButtons/ChoosePrefsWithButtons.tsx
+++ src/components/ChoosePrefsWithButtons/ChoosePrefsWithButtons.tsx
@@ -1,16 +1,37 @@
 // import { Title, Text, Anchor } from '@mantine/core';
+import { PropsWithChildren } from 'react';
 import { Button, TextInput, rem } from '@mantine/core';
-import { IconSearch } from '@tabler/icons-react';
+import { IconSearch, IconCheck } from '@tabler/icons-react';
 
 import classes from './ChoosePrefsWithButtons.module.css';
 import mantineButtonStyles from './mantinePrefsButton.module.css';
-import { MockAreas, MockArea } from '../../data/mock/mock-areas';
+// import { MockArea } from '../../data/mock/mock-areas';
 
-export function ChoosePrefsWithButtons() {
-  const icon = (
+export function ChoosePrefsWithButtons({
+  dataItems,
+  selectedDataItems,
+  onSelectDataItem,
+  searchedPref,
+  onChangeSearchPref,
+}: PropsWithChildren<{
+  dataItems: any[];
+  selectedDataItems?: any[];
+  onSelectDataItem?: (selectedDataItem: any) => any;
+  searchedPref?: string;
+  onChangeSearchPref?: (searchPrefChanged: string) => any;
+}>) {
+  console.log('ChoosePrefsWithButtons dataItems', dataItems);
+  const iconSearch = (
     <IconSearch style={{ width: rem(24), height: rem(24) }} stroke="1.5" color="black" />
   );
-  const areas: MockArea[] = MockAreas;
+  const iconPrefButtonSel = (
+    <IconCheck style={{ width: rem(24), height: rem(24) }} stroke="1.5" color="#9BF57B" />
+  );
+  const isDataItemSelected = (dataItem: any) =>
+    Array.isArray(selectedDataItems) &&
+    selectedDataItems.find(
+      (selDataItem) => dataItem.termCode && dataItem.termCode === selDataItem.termCode
+    );
 
   return (
     <>
@@ -18,20 +39,42 @@ export function ChoosePrefsWithButtons() {
         <TextInput
           className={classes.searchInput}
           leftSectionPointerEvents="none"
-          leftSection={icon}
+          leftSection={iconSearch}
           placeholder="Rechercher une ville ou code postal"
+          value={searchedPref}
+          onChange={(event) =>
+            onChangeSearchPref ? onChangeSearchPref(event.currentTarget.value) : null
+          }
         />
         <div className={classes.prefButtons}>
-          {areas.map((area) => (
-            <Button
-              variant="outline"
-              radius="xl"
-              className={classes.prefButton}
-              classNames={mantineButtonStyles}
-            >
-              {area.name}
-            </Button>
-          ))}
+          {dataItems.map((dataItem) => {
+            const isItemSel: boolean = isDataItemSelected(dataItem);
+            return (
+              <Button
+                key={dataItem.termCode || dataItem._id}
+                variant={isItemSel ? 'filled' : 'outline'}
+                radius="xl"
+                leftSection={isItemSel ? iconPrefButtonSel : null}
+                className={classes.prefButton}
+                classNames={
+                  isItemSel
+                    ? {
+                        root: mantineButtonStyles.root_sel,
+                        inner: mantineButtonStyles.inner_sel,
+                        label: mantineButtonStyles.label_sel,
+                      }
+                    : {
+                        root: mantineButtonStyles.root_not_sel,
+                        inner: mantineButtonStyles.inner_not_sel,
+                        label: mantineButtonStyles.label_not_sel,
+                      }
+                }
+                onClick={() => onSelectDataItem(dataItem)}
+              >
+                {dataItem.name}
+              </Button>
+            );
+          })}
           {/* <Button
             variant="outline"
             radius="xl"
@@ -40,6 +83,19 @@ export function ChoosePrefsWithButtons() {
           >
             Button 1 serfg ewrg weg wertgh getryh wergw erth wwerfq ert werf qwert e tyuktyujrt
           </Button> */}
+        </div>
+        <div className={classes.prefButtons}>
+          {selectedDataItems.map((selDataItem) => (
+            <Button
+              key={selDataItem.termCode || selDataItem._id}
+              variant="outline"
+              radius="xl"
+              className={classes.prefButton}
+              classNames={mantineButtonStyles}
+            >
+              {selDataItem.name}
+            </Button>
+          ))}
         </div>
       </div>
     </>
--- src/pages/PreferredAreas.page.tsx
+++ src/pages/PreferredAreas.page.tsx
@@ -1,13 +1,58 @@
 // import { Welcome } from '../components/Welcome/Welcome';
 // import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { useState } from 'react';
 import classes from './PreferredAreas.module.css';
 
 import { CitizenPageFrame } from '@/components/CitizenPageFrame/CitizenPageFrame';
 import { CitizenHeader } from '../components/CitizenHeader/CitizenHeader';
 import { CitizenCreateProfileProgressBar } from '../components/CitizenCreateProfileProgressBar/CitizenCreateProfileProgressBar';
 import { ChoosePrefsWithButtons } from '../components/ChoosePrefsWithButtons/ChoosePrefsWithButtons';
+import { MockAreas, MockArea } from '../data/mock/mock-areas';
 
 export function PreferredAreas() {
+  const prefs: MockArea[] = MockAreas;
+  const [selectedDataItems, setSelectedDataItems] = useState([]);
+  const [searchedPref, setSearchedPref] = useState('');
+  let filteredPrefs: MockArea[] = prefs;
+
+  const onChangeSearchPref = (changedSearchPref: string) => {
+    console.log('PreferredAreas onChangeSearchPref changedSearchPref', changedSearchPref);
+    filteredPrefs = prefs.filter((pref: MockArea) => {
+      const cPref: string = changedSearchPref ? changedSearchPref.trim().toLowerCase() : '';
+      if (!cPref) {
+        return true;
+      }
+      return pref.name.trim().toLowerCase().includes(cPref);
+    });
+    console.log('PreferredAreas onChangeSearchPref filteredPrefs', filteredPrefs);
+    setSearchedPref(`${changedSearchPref}`);
+  };
+
+  const onSelectDataItem = (selectedDataItem: any) => {
+    if (
+      !selectedDataItems.find(
+        (alreadySelItem) =>
+          alreadySelItem.termCode && alreadySelItem.termCode === selectedDataItem.termCode
+      )
+    ) {
+      setSelectedDataItems([...selectedDataItems, selectedDataItem]);
+    } else {
+      setSelectedDataItems([
+        ...selectedDataItems.filter(
+          (alreadySelItem) =>
+            alreadySelItem.termCode && alreadySelItem.termCode !== selectedDataItem.termCode
+        ),
+      ]);
+    }
+    // selectedDataItems.push(selectedDataItem);
+    console.log(
+      'PreferredAreas selectedDataItems: ',
+      selectedDataItems,
+      ' selectedDataItem: ',
+      selectedDataItem
+    );
+  };
+
   return (
     <>
       <CitizenPageFrame>
@@ -19,7 +64,13 @@ export function PreferredAreas() {
             Sélectionnez les villes, régions et provinces où vous profitez habituellement de vos
             activités culturelles.
           </div>
-          <ChoosePrefsWithButtons />
+          <ChoosePrefsWithButtons
+            dataItems={filteredPrefs}
+            selectedDataItems={selectedDataItems}
+            onSelectDataItem={onSelectDataItem}
+            searchedPref={searchedPref}
+            onChangeSearchPref={onChangeSearchPref}
+          />
         </div>
       </CitizenPageFrame>
     </>
